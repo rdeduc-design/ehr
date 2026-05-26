e0a67b445969776b93e1f71064680db80745a6ee
rdeduc-design <rdeduc@hct.ph>
2026-05-26 10:05:24 +0800

    Remove obsolete online HIS rerender overlay

--- hospital-online-rerender.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  function refreshHis() {
-    try {
-      if (window.state && window.state.tab === "his" && typeof window.render === "function") window.render();
-    } catch (_) {}
-  }
-  if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", refreshHis);
-  else refreshHis();
-})();
